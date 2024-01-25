4e1c5b5dd2748d582cb571c56f6c205fe9e982f1
smhrd <smhrd@59.0.129.84>
2024-01-25 12:28:19 +0900

    블롭피쉬 쓰러트리기

--- Gittest01/src/kinghoonclass.java
+++ Gittest01/src/kinghoonclass.java
@@ -18,7 +18,7 @@ public class kinghoonclass {
 		 if(axpower>30) {
 			 System.out.println("데미지"+axpower+"치명적인 피해를 입혔다!!");
 		 }
-		  cnt++;
+		  cnt++;  
 		}System.out.println(cnt+"번만에 블롭피쉬를 쓰러트렸다");
 		}
 	}
