5c06ff4d3071f919f53fa81a422579144cd25f31
smhrd <smhrd@59.0.129.84>
2024-01-25 11:30:39 +0900

    네번째커밋

--- Gittest01/src/Ex01tigerTeam.java
+++ Gittest01/src/Ex01tigerTeam.java
@@ -5,6 +5,7 @@ public class Ex01tigerTeam {
 		System.out.println("팀장 첫번째 커밋!");
 		System.out.println("팀원1 두번째 커밋");
 		System.out.println("팀장 세번째 커밋");
+		System.out.println("팀원2 네번째 커밋");
 		
 
 	}
