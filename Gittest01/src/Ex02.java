0fdd93607fcb26e578449ec91f80ac315e8d3969
smhrd <smhrd@222.102.104.218>
2024-01-25 12:18:30 +0900

    임지훈 쾌남 자격 박탈

---
+++ Gittest01/src/Ex02.java
@@ -0,0 +1,24 @@
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		Boolean loop = true;
+		while(loop) {
+			for(int i=1;i<=5;i++) {
+				System.out.println("임지훈은 쾌남 중의 쾌남 입니다");
+				if(i==5) {
+					loop = false;
+					System.out.println("이제 임지훈 님은 더이상 쾌남이 아닙니다.");
+				}
+			}
+			
+			
+			
+			
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
