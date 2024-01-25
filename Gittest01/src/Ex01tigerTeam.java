e8e9b58e1ec449510f44f7c3059a1f6cca4577f7
smhrd <smhrd@222.102.104.218>
2024-01-25 11:28:41 +0900

    세번째 커밋

--- Gittest01/src/Ex01tigerTeam.java
+++ Gittest01/src/Ex01tigerTeam.java
@@ -4,6 +4,7 @@ public class Ex01tigerTeam {
 	public static void main(String[] args) {
 		System.out.println("팀장 첫번째 커밋!");
 		System.out.println("팀원1 두번째 커밋");
+		System.out.println("팀장 세번째 커밋");
 		
 
 	}
