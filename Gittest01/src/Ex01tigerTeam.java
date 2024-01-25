36ebbde8f8533f0d8758c2f029703e4aad703fb4
smhrd <smhrd@222.102.104.218>
2024-01-25 11:36:51 +0900

    임지훈 팬카페 팀장

--- Gittest01/src/Ex01tigerTeam.java
+++ Gittest01/src/Ex01tigerTeam.java
@@ -6,13 +6,9 @@ public class Ex01tigerTeam {
 		System.out.println("팀원1 두번째 커밋");
 		System.out.println("팀장 세번째 커밋");
 		System.out.println("팀원2 네번째 커밋");
-		System.out.println("팀원1 다섯번째 커밋");
-		
+		System.out.println("임지훈,,,,,,멋있다 !!");
 		
 
-		
-	
-		
 	}
 
 }
