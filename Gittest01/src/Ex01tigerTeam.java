7aea4eff125884b1e342b98d11b17bafcfbdf944
smhrd <smhrd@59.0.129.84>
2024-01-26 10:26:46 +0900

    초기화 커밋

--- Gittest01/src/Ex01tigerTeam.java
+++ Gittest01/src/Ex01tigerTeam.java
@@ -2,15 +2,6 @@
 public class Ex01tigerTeam {
 
 	public static void main(String[] args) {
-		System.out.println("팀장 첫번째 커밋!");
-		System.out.println("팀원1 두번째 커밋");
-		System.out.println("팀장 세번째 커밋");
-		System.out.println("팀원2 네번째 커밋");
-		System.out.println("임지훈,,,,,,멋있다 !!");
-		System.out.println("사랑이 넘 귀여오오옹");
-		System.out.println("알밤이는 삵인가요");
-		System.out.println("무늬와 모양으로 보아 삵으로 판단되어집니다.");
-		System.out.println("블롭피쉬ㅌ");
 		
 		
 		
