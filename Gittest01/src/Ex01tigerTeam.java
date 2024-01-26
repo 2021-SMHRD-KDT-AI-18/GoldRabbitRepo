bc5ff6ba68e5f4b78a1ede78e4625f1b89c95bbb
smhrd <smhrd@222.102.104.218>
2024-01-26 10:50:01 +0900

    게임스토리 설명

--- Gittest01/src/Ex01tigerTeam.java
+++ Gittest01/src/Ex01tigerTeam.java
@@ -22,6 +22,27 @@ public class Ex01tigerTeam {
 		
 		
 		
-	}
-
+		String text = " 평화로운 쿠키마을에 사는 용맹한 전사 쿠키는 어느 날, 악당 초코몬스터와 그의 부하들이 공격해왔다. 마을은 파괴되고 주민들은 위협받고 있었다."
+				+" 쿠키는 자신의 마을을 지키기 위해 전사로 일어나 악당에 맞서기로 결심한다. "
+				+"그는 특별한 능력과 함께 무기를 갖추고, 모험과 도전 속에서 성장하며 친구들과 협력하여 마을을 되찾는 여정에 나선다. "
+				+"악당을 물리치고 마을에 다시 평화가 찾아오게끔, 쿠키는 용감한 용사로서의 자부심을 얻는다.\r\n";
+		
+		
+		for(int i =0;i<text.length();i++) {
+			System.out.print(text.charAt(i));
+			
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} if(i%45==0) {
+				System.out.println();
+				
+			}
+			
+		}
+	} 
+	
 }
+
